7b04a0755c44df7456c80d9ece4b0398903838ba
Jiawen Ge <jiawen_kevin_ge@gatech.edu>
2019-03-30 10:35:44 -0400

    backend query updated

--- backend/ANN.py
+++ backend/ANN.py
@@ -1,14 +1,23 @@
 from sklearn.externals import joblib
+import json
+import numpy as np
 
 clf = joblib.load('ANN.joblib')
 data2 = np.load('data2.npy')
 header = data2.dtype.names
 
-def predict(csv):
-    data_new = np.genfromtxt(csv, dtype=None,
-        delimiter=',', names=True, filling_values=0.0)
-    y = data_new[header[0]]
-    x = np.zeros((len(y), len(header) - 1))
+def predict(json_input):
+    table = json.loads(json_input)
+    data_new = []
+    for row in table:
+        row_new = []
+        for label in header:
+            if label != 'Churn':
+                row_new.append(row[label])
+        data_new.append(row_new)
+    data_new = np.array(data_new)
+
+    x = np.zeros((len(y), len(header)))
 
     for i, h in enumerate(header):
         if h != 'Churn':
