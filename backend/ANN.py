d1a885eaeca7175edc0cd284b56c9377032e33c0
Jiawen Ge <jiawen_kevin_ge@gatech.edu>
2019-03-30 11:53:47 -0400

    Backend query tested

--- backend/ANN.py
+++ backend/ANN.py
@@ -7,16 +7,16 @@ data2 = np.load('data2.npy')
 header = data2.dtype.names
 
 def predict(json_input):
-    table = json.loads(json_input)
+    table = json.load(json_input)
     count = 0
     data = []
     for row in table:
         if count == 0:
             header = row.keys()
-            data.append(list(header))
             count += 1
         data.append(list(row.values()))
     data_new = np.array(data)
+    data_new.dtype = [(label, data_new.dtype) for label in header]
 
     x = np.zeros((len(table), len(header)))
 
@@ -30,9 +30,10 @@ def predict(json_input):
                 dat = np.zeros(raw_new.shape)
                 vals = np.unique(raw)
                 for j, u in enumerate(vals):
-                    dat[raw_new == u] = j
+                    dat[raw_new == u.decode('UTF-8')] = j
             if (np.isnan(dat).any()):
                 print(dat)
                 print(h)
-            x[:,i] = dat
+            for row in range(len(dat)):
+                x[row][i] = dat[row][0]
     return clf.predict(x)
